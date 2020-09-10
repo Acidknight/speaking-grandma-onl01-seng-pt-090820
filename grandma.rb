3aadfe02f6287a90b3dad67fed72db9567f0eb60
organized-key-8464 <acidknights89@gmail.com>
2020-09-10 03:24:27 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -20,7 +20,7 @@ def speak_to_grandma(phrase)
     return "HUH?! SPEAK UP, SONNY!"
     elsif phrase == "WHAT?"
     return "NO, NOT SINCE 1938!"
-  else "NO< NOT SINCE 1938"
+  else "NO, NOT SINCE 1938!"
     
   end
 end
