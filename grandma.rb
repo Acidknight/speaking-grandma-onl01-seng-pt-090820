358df83eb3a1dd45df49d8a360c7c5282055ebea
organized-key-8464 <acidknights89@gmail.com>
2020-09-10 03:21:17 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -17,8 +17,9 @@ def speak_to_grandma(phrase)
     elsif phrase == "Hi Nana, how are you?"
     return "HUH?! SPEAK UP, SONNY!"
     elsif phrase == "Hi!"
+    return "HUH?! SPEAK UP, SONNY!"
     elsif phrase == "WHAT?"
     return "NO, NOT SINCE 1938!"
-    return "HUH?! SPEAK UP, SONNY!"
+    
   end
 end
